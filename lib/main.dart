760f5a13da1bf5cb9ab08fccf36a92f51db8ccd0
Jaimul Islam Akik <105303801+devjaimul@users.noreply.github.com>
2024-04-19 17:17:55 +0600

    c

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,43 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //ai 2 vabei kaj kora jay
+
+    // old
+    print(MediaQuery.of(context).displayFeatures);
+
+    //new
+    print(MediaQuery.displayFeaturesOf(context));
+
+    print(MediaQuery.sizeOf(context));
+    print(MediaQuery.sizeOf(context).width);
+    print(MediaQuery.sizeOf(context).height);
+    print(MediaQuery.orientationOf(context));
+    print(MediaQuery.devicePixelRatioOf(context));
+
     return Scaffold(
-body: ,
+      body: OrientationBuilder(builder: (context, orientation) {
+        if(orientation==Orientation.portrait){
+          return Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Colors.teal,
+          );
+        }
+        else {
+          return Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Colors.blue.shade300,
+          );
+        }
+      },),
     );
   }
 }
